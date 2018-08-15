cd31b4aaa7d1a7a2a698efa15ec1035dd1ff7987
jackhea <kbpmvb@hotmail.com>
2018-08-14 20:10:41 -0500

    found bug and fixed it -31

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ class App extends Component {
 		this.videoSearch('nintendo switch');
 	}
 
-	videoSearch(term) {
+	videoSearch(term){
 		YTSearch({ key: API_KEY, term: term}, (videos) => { 
 			this.setState({ 
 				videos: videos,
@@ -34,10 +34,10 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
-				<VideoDetail video={this.state.selectedVideo}/>
+				<SearchBar onSearchTermChange= {term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
-				onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+				onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
 				videos={this.state.videos}/>
 			</div>
 			);
