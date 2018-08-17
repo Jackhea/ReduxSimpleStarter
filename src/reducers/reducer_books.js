bddc10f2f495f2d6f691543eeff0b318198ab7a5
jackhea <kbpmvb@hotmail.com>
2018-08-17 16:16:21 -0500

    finished 2nd project -51

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+	{title: 'Javascript the good parts', pages: '223'},
+	{title: 'Harry Potter', pages: '214'},
+	{title: 'The dark tower', pages: '842'},
+	{title: 'Eloquent ruby', pages: '1'}
+	]
+}
